d89de3a88750b53cac08afa494e90c707c29fd74
Ayman Abdelaziz <you@example.com>
2023-03-31 07:08:17 +0200

    [Enhance] : enhance some styling

--- src/modules/dashboard/pages/Dashboard.tsx
+++ src/modules/dashboard/pages/Dashboard.tsx
@@ -3,7 +3,6 @@ import { getAllTickets } from "../api/dashboard-api";
 import AddNewTicketButton from "../containers/AddNewTicketButton";
 import TicketsList from "../containers/TicketsList";
 import { ITicket } from "../types/dashboard-types";
-import styles from "./Dashboard.module.scss";
 
 const Dashboard = () => {
   const [tickets, setTickets] = useState<ITicket[]>([]);
@@ -17,10 +16,10 @@ const Dashboard = () => {
   }, [fetchTickets]);
 
   return (
-    <div className={styles.Wrapper}>
+    <>
       <AddNewTicketButton fetchTickets={fetchTickets} />
       <TicketsList tickets={tickets} fetchTickets={fetchTickets} />
-    </div>
+    </>
   );
 };
 
