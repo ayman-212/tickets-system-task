16e6cb27918c64129d87d1edb4bbb6588b4c508a
Ayman Abdelaziz <you@example.com>
2023-03-31 12:43:35 +0200

    [Change] : change App.tsx and index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Dashboard from "./modules/dashboard/pages/Dashboard";
+import Layout from "./modules/layout/pages/Layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <Layout>
+        <Dashboard />
+      </Layout>
+    </>
   );
 }
 
