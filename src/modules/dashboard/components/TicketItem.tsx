a9a143ef0ac2a5de1b62f62b5492c321dc281800
Ayman Abdelaziz <you@example.com>
2023-03-31 12:39:59 +0200

    [Create] (TicketItem.tsx) : create component which includes all details about ticket

---
+++ src/modules/dashboard/components/TicketItem.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { ITicket } from "../types/dashboard-types";
+import styles from "./TicketItem.module.scss";
+import { ReactComponent as DeleteIcon } from "../../../assets/delete-bin.svg";
+import { deleteTicket } from "../api/dashboard-api";
+import { toast } from "react-toastify";
+
+type TicketItemProp = {
+  ticket: ITicket;
+  fetchTickets: () => void;
+};
+
+const TicketItem = ({ ticket, fetchTickets }: TicketItemProp) => {
+  const { subject, description, priority, status, id } = ticket;
+
+  const deleteTicketHandler = () => {
+    deleteTicket(id)
+      .then(() => {
+        toast.success("The task has been deleted successfully", {
+          position: "bottom-right",
+        });
+        fetchTickets();
+      })
+      .catch(() => {
+        toast.error("Something went wrong, please try again later", {
+          position: "top-right",
+        });
+      });
+  };
+
+  return (
+    <div className={styles.TicketItemContainer}>
+      <div className={styles.TitleWrapper}>
+        <h3>{subject}</h3>
+        <span onClick={deleteTicketHandler}>
+          <DeleteIcon />
+        </span>
+      </div>
+      <p>{description}</p>
+      <div className={styles.PriorityAndStatueWrapper}>
+        <span className={styles[priority]}>{priority}</span>
+        <span className={styles[status]}>{status}</span>
+      </div>
+    </div>
+  );
+};
+
+export default TicketItem;
